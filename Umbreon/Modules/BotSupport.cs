f5961f4a747fbffc8be91783b1a1227399a55ba4
Casino Boyale <kayrenhi@gmail.com>
2018-07-31 01:59:38 +0100

    Removed unnecessary task spawning

--- Umbreon/Modules/BotSupport.cs
+++ Umbreon/Modules/BotSupport.cs
@@ -18,27 +18,27 @@ namespace Umbreon.Modules
         [Name("Bug Report")]
         [Summary("Submit a bug report. Please be as informative as possible")]
         [Usage("bot bug Umbreon is 2 cwl")]
-        public async Task BugReport(
+        public Task BugReport(
             [Name("Report")]
             [Summary("The bug, as descriptive as possible please")]
             [Remainder] string report)
-            => await (Context.Client.GetChannel(463299724326469634) as SocketTextChannel).SendMessageAsync($"{DateTime.UtcNow.TimeOfDay} : {Context.User.GetDisplayName()} : {Context.Guild.Name} : {Context.Channel.Name}({Context.Channel.Id}) - {report}");
+            => (Context.Client.GetChannel(463299724326469634) as SocketTextChannel).SendMessageAsync($"{DateTime.UtcNow.TimeOfDay} : {Context.User.GetDisplayName()} : {Context.Guild.Name} : {Context.Channel.Name}({Context.Channel.Id}) - {report}");
 
         [Command("Feature")]
         [Name("Feature Request")]
         [Summary("Submit a feature request")]
         [Usage("bot feature make umbreon cwler")]
-        public async Task FeatureReq(
+        public Task FeatureReq(
             [Name("Request")]
             [Summary("The feature that you want")]
             [Remainder] string feature)
-            => await (Context.Client.GetChannel(463300066740797463) as SocketTextChannel).SendMessageAsync($"{DateTime.UtcNow.TimeOfDay} : {Context.User.GetDisplayName()} : {Context.Guild.Name} : {Context.Channel.Name}({Context.Channel.Id}) - {feature}");
+            => (Context.Client.GetChannel(463300066740797463) as SocketTextChannel).SendMessageAsync($"{DateTime.UtcNow.TimeOfDay} : {Context.User.GetDisplayName()} : {Context.Guild.Name} : {Context.Channel.Name}({Context.Channel.Id}) - {feature}");
 
         [Command("Source")]
         [Name("Bot Source")]
         [Summary("The source code for the bot")]
         [Usage("bot source")]
-        public async Task GetSource()
-            => await SendMessageAsync("https://github.com/purpledank/Umbreon");
+        public Task GetSource()
+            => SendMessageAsync("https://github.com/purpledank/Umbreon");
     }
 }
