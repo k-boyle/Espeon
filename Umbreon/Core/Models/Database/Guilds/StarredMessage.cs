a772dfced4361cb2d5f21c8867a22e34bc707a23
Casino Boyale <kayrenhi@gmail.com>
2018-08-01 12:46:22 +0100

    Removal of remaining Starboard items

--- Umbreon/Core/Models/Database/Guilds/StarredMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Umbreon.Core.Models.Database
-{
-    public class StarredMessage
-    {
-        public ulong MessageId { get; set; }
-        public ulong StarMessageId { get; set; }
-        public int StarCount { get; set; }
-    }
-}
