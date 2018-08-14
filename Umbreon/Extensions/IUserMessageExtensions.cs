412864646d5c47ca0ec605ffef705dffc9d142f8
Casino Boyale <kayrenhi@gmail.com>
2018-08-14 15:42:14 +0100

    Forgot the reaction option

--- Umbreon/Extensions/IUserMessageExtensions.cs
+++ Umbreon/Extensions/IUserMessageExtensions.cs
@@ -11,6 +11,9 @@ namespace Umbreon.Extensions
         public static string GetJumpLink(this IUserMessage message) => $"https://discordapp.com/channels/{(message.Channel as IGuildChannel).Guild.Id}/{message.Channel.Id}/{message.Id}";
 
         public static Task AddReactionsAsync(this IUserMessage msg, IEnumerable<IEmote> emotes)
-            => Task.WhenAll(emotes.Select(x => msg.AddReactionAsync(x)));
+            => Task.WhenAll(emotes.Select(x => msg.AddReactionAsync(x, new RequestOptions
+            {
+                BypassBuckets = true
+            })));
     }
 }
