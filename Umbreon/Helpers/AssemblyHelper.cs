e433da8b59ae60df41ea9c8f2e36e3f77f796356
Casino Boyale <kayrenhi@gmail.com>
2018-09-15 23:26:53 +0100

    Stupid Sid

--- Umbreon/Helpers/AssemblyHelper.cs
+++ Umbreon/Helpers/AssemblyHelper.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Umbreon.Helpers
 {
     public static class AssemblyHelper
     {
         public static IEnumerable<Type> GetAllTypesWithAttribute<T>() where T : Attribute
-            => AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
-                .Where(y => y.GetCustomAttributes(typeof(T), true).Length > 0);
+            => Assembly.GetEntryAssembly().GetTypes().Where(y => y.GetCustomAttributes(typeof(T), true).Length > 0);
     }
 }
