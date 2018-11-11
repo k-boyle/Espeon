96233979a0f0968939d64e31402a559b3c85a4a2
Casino Boyale <kayrenhi@gmail.com>
2018-11-11 13:12:32 +0000

    Added in command Aliases

--- Espeon.Core/Commands/Modules/Management.cs
+++ Espeon.Core/Commands/Modules/Management.cs
@@ -15,14 +15,33 @@ namespace Espeon.Core.Commands.Modules
         public override async Task<EspeonResult> AddAsync(Module target, string value)
         {
             var result = await Manager.AddAliasAsync(target, value);
-            return new EspeonResult(result, result ? "Alias has been added!" : "Alias already exists for this module!");
+            return new EspeonResult(result, result ? $"{value} has been added to {target.Name}!" : $"{value} already exists for {target.Name}!");
         }
 
         [Command("Remove")]
         public override async Task<EspeonResult> RemoveAsync(Module target, string value)
         {
             var result = await Manager.RemoveAliasAsync(target, value);
-            return new EspeonResult(result, result ? "Alias has been removed!" : "Alias doesn't exist for this module!");
+            return new EspeonResult(result, result ? $"{value} has been removed from {target.Name}!" : $"{value} doesn't exist for {target.Name}!");
+        }
+    }
+
+    [Name("Command Management")]
+    [Group("Command")]
+    public class CommandManagement : AddRemoveBase<Command, string>
+    {
+        [Command("Add")]
+        public override async Task<EspeonResult> AddAsync(Command target, string value)
+        {
+            var result = await Manager.AddAliasAsync(target.Module, target, value);
+            return new EspeonResult(result, result ? $"{value} has been added to {target.Name}!" : $"{value} already exists for {target.Name}!");
+        }
+
+        [Command("Remove")]
+        public override async Task<EspeonResult> RemoveAsync(Command target, string value)
+        {
+            var result = await Manager.RemoveAliasAsync(target.Module, target, value);
+            return new EspeonResult(result, result ? $"{value} has been removed from {target.Name}!" : $"{value} doesn't exist for {target.Name}!");
         }
     }
 }
