6b04bf21ca35d4073a46e744dcbb7ef1161495f5
TheCasino <kayrenhi@gmail.com>
2018-12-05 18:24:03 +0000

    I'm an active dev

--- Espeon.Core/Services/IResponseService.cs
+++ Espeon.Core/Services/IResponseService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Qmmands;
 
@@ -5,6 +6,7 @@ namespace Espeon.Core.Services
 {
     public interface IResponseService
     {
-        Task<string> GetResponseAsync(Module module, Command command);
+        Task<string> GetResponseAsync(Module module, Command command, string pack, params string[] @params);
+        Task OnCommandsRegisteredAsync(IEnumerable<Module> modules);
     }
 }
--- Espeon/Services/ResponseService.cs
+++ Espeon/Services/ResponseService.cs
@@ -12,43 +12,55 @@ namespace Espeon.Services
     [Service(typeof(IResponseService), true)]
     public class ResponseService : IResponseService
     {
-        [Inject] private readonly CommandService _commands;
-
         private const string MapDir = "./commands.json";
 
-        private readonly IDictionary<string, Dictionary<string, string[]>> _responseMap;
+        private readonly IDictionary<string, Dictionary<string, Dictionary<string, string>>> _responseMap;
 
         public ResponseService()
-        {
-            if (File.Exists(MapDir))
-            {
-                _responseMap =
-                    JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string[]>>>(
-                        File.ReadAllText(MapDir));
-            }
-            else
-            {
-                _responseMap = new Dictionary<string, Dictionary<string, string[]>>();
-            }
-
-            _commands.ModuleBuilding += OnModuleBuildingAsync;
+        {            
+            _responseMap = new Dictionary<string, Dictionary<string, Dictionary<string, string>>>();
         }
 
-        private async Task OnModuleBuildingAsync(ModuleBuilder moduleBuilder)
+        public async Task<string> GetResponseAsync(Module module, Command command, string pack = "default", params string[] @params)
         {
-            if (_responseMap.ContainsKey(moduleBuilder.Name))
-            {
-
-            }
-            else
-            {
-                
-            }
+            var response = _responseMap[module.Name][command.Name][pack];
+            //TODO interpolation
+            return response ?? "No response found";
         }
 
-        public async Task<string> GetResponseAsync(Module module, Command command)
+        public Task OnCommandsRegisteredAsync(IEnumerable<Module> modules)
         {
-            throw new NotImplementedException();
+            var loadedMap = JsonConvert
+                .DeserializeObject<Dictionary<string, Dictionary<string, Dictionary<string, string>>>>
+                (File.ReadAllText(MapDir));
+
+            foreach(var module in modules)
+            {
+                if(string.IsNullOrWhiteSpace(module.Name))
+                    throw new ArgumentNullException(nameof(module.Name));
+
+                if(!loadedMap.ContainsKey(module.Name))
+                    loadedMap[module.Name] = new Dictionary<string, Dictionary<string, string>>();
+
+                _responseMap.Add(module.Name, new Dictionary<string, Dictionary<string, string>>());
+
+                var commands = module.Commands;
+
+                foreach(var command in commands)
+                {
+                    if(string.IsNullOrWhiteSpace(command.Name))
+                        throw new ArgumentNullException(nameof(command.Name));                    
+
+                    if(!loadedMap[module.Name].ContainsKey(command.Name))
+                        loadedMap[module.Name][command.Name] = new Dictionary<string, string>();
+
+                    _responseMap[module.Name][command.Name] = loadedMap[module.Name][command.Name];
+                }
+            }
+
+            File.WriteAllText(MapDir, JsonConvert.SerializeObject(loadedMap));
+
+            return Task.CompletedTask;
         }
     }
 }
