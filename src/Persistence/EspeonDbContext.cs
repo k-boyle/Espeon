e5a6bfd32ce72a881267ca89c7d923e5817e4750
Kieran Boyle <kayrenhi@gmail.com>
2020-04-09 17:22:33 +0100

    Added initial persistence
    Signed-off-by: Casino Boyale <kayrenhi@gmail.com>

---
+++ src/Persistence/EspeonDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Disqord;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Espeon.Persistence {
+    public class EspeonDbContext : DbContext {
+        private DbSet<GuildPrefixes> GuildPrefixes { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+            => optionsBuilder.UseNpgsql("Host=127.0.0.1;Port=5432;Database=Espeon;Username=postgres;Password=casino");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<GuildPrefixes>(model => {
+                model.HasIndex(prefixes => prefixes.GuildId).IsUnique();
+                model.Property(prefixes => prefixes.GuildId).ValueGeneratedNever();
+            });
+        }
+
+        public async Task<GuildPrefixes> GetPrefixesAsync(IGuild guild) {
+            return await GuildPrefixes.SingleOrDefaultAsync(prefix => prefix.GuildId == guild.Id);
+        }
+
+        public async Task PersistGuildAsync(IGuild guild) {
+            await GuildPrefixes.AddAsync(new GuildPrefixes(guild.Id));
+            await SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Persistence/GuildPrefixes.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Espeon.Persistence {
+    [Table("prefixes")]
+    public class GuildPrefixes {
+        [Key]
+        [Column("guild_id")]
+        public ulong GuildId { get; set; }
+        
+        [Required]
+        [Column("prefixes")]
+        public string[] Prefixes { get; set; }
+
+        public GuildPrefixes(ulong guildId) {
+            GuildId = guildId;
+            Prefixes = new[] {
+                "es/"
+            };
+        }
+    }
+}
