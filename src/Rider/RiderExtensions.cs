f6e03aaa145335bb1607dade85f508b1de55323e
Casino Boyale <kayrenhi@gmail.com>
2020-04-11 17:39:18 +0100

    Added is extension
    Signed-off-by: Casino Boyale <kayrenhi@gmail.com>

--- src/Rider/RiderExtensions.cs
+++ src/Rider/RiderExtensions.cs
@@ -7,5 +7,13 @@ namespace Espeon {
         public static void slog(this ILogger logger) {
             //$ this._logger = logger.ForContext("SourceContext", GetType().Name);
         }
+        
+        [SourceTemplate]
+        [Macro(Target = "type", Expression = "fixedTypeName()")]
+        public static void @is(this object obj) {
+            /*$ if (obj is type) {
+                $END$
+             }*/
+        }
     }
 }
