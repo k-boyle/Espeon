3aff9639531f3600222e0ff1c44d1b6fdec4388e
Casino Boyale <kayrenhi@gmail.com>
2020-05-03 21:25:37 +0100

    Commands helper class

---
+++ src/Commands/CommandHelpers.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace Espeon {
+    public static class CommandHelpers {
+        public static async Task GlobalTagCallback(EspeonCommandContext context) {
+            await using var dbContext = context.ServiceProvider.GetService<EspeonDbContext>();
+            var tag = await dbContext.GetTagAsync<GlobalTag>(context.Command.Name);
+            await context.Channel.SendMessageAsync(tag.Value);
+            tag.Uses++;
+            await dbContext.UpdateAsync(tag);
+        }
+    }
+}
