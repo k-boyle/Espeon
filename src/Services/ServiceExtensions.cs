ca4ecf73ce4046b283770d67ac6650ef62201ec5
Casino Boyale <kayrenhi@gmail.com>
2020-10-13 21:51:32 +0100

    Changed to use fluent api

--- src/Services/ServiceExtensions.cs
+++ src/Services/ServiceExtensions.cs
@@ -5,9 +5,8 @@ namespace Espeon {
     public static class ServiceExtensions {
         public static IServiceCollection AddTHostedService<TService>(this IServiceCollection serviceCollection)
                 where TService : class, IHostedService {
-            serviceCollection.AddSingleton<TService>();
-            serviceCollection.AddHostedService(provider => provider.GetRequiredService<TService>());
-            return serviceCollection;
+            return serviceCollection.AddSingleton<TService>()
+                .AddHostedService(provider => provider.GetRequiredService<TService>());
         }
     }
 }
